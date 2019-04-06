392b417471d99add27c56f9929cb740a9e8a141e
Victor Azevedo <victor_mouraazevedo@hotmail.com>
2019-04-06 19:23:50 -0300

    inserção de um Toast na Activity

--- app/src/main/java/br/gov/sp/fatec/agenda/MainActivity.java
+++ app/src/main/java/br/gov/sp/fatec/agenda/MainActivity.java
@@ -1,6 +1,15 @@
 package br.gov.sp.fatec.agenda;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Toast.makeText(this, "Victor Moura Azevedo", Toast.LENGTH_LONG).show();
+    }
 }
