cb7840f3dddaeb2434048b35b063b50dbe23acd7
Victor Azevedo <victor_mouraazevedo@hotmail.com>
2019-04-06 20:58:46 -0300

    inserção da Lista no layout

--- app/src/main/java/br/gov/sp/fatec/agenda/MainActivity.java
+++ app/src/main/java/br/gov/sp/fatec/agenda/MainActivity.java
@@ -3,6 +3,11 @@ package br.gov.sp.fatec.agenda;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends Activity {
 
@@ -10,5 +15,13 @@ public class MainActivity extends Activity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        List<String> alunos = new ArrayList<>(
+                Arrays.asList("Victor", "Serena", "Marilia"));
+        TextView primeiroAluno = findViewById(R.id.textView);
+        TextView segundoAluno = findViewById(R.id.textView2);
+        TextView terceiroAluno = findViewById(R.id.textView3);
+        primeiroAluno.setText(alunos.get(0));
+        segundoAluno.setText(alunos.get(1));
+        terceiroAluno.setText(alunos.get(2));
     }
 }
