b3acae3531afce3c350af9d65cc18ba73090fe8c
Victor Azevedo <victor_mouraazevedo@hotmail.com>
2019-04-07 16:23:26 -0300

    Implementação para Salvar Aluno

--- app/src/main/java/br/gov/sp/fatec/agenda/ui/activity/FormularioAlunoActivity.java
+++ app/src/main/java/br/gov/sp/fatec/agenda/ui/activity/FormularioAlunoActivity.java
@@ -1,9 +1,15 @@
 package br.gov.sp.fatec.agenda.ui.activity;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import br.gov.sp.fatec.agenda.R;
+import br.gov.sp.fatec.agenda.dao.AlunoDAO;
+import br.gov.sp.fatec.agenda.model.Aluno;
 
 public class FormularioAlunoActivity extends AppCompatActivity {
 
@@ -12,5 +18,27 @@ public class FormularioAlunoActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setTitle("Novo aluno");
         setContentView(R.layout.activity_formulario_aluno);
+
+        final AlunoDAO dao = new AlunoDAO();
+
+        final EditText campoNome = findViewById(R.id.activity_formulario_aluno_nome);
+        final EditText campoTelefone = findViewById(R.id.activity_formulario_aluno_telefone);
+        final EditText campoEmail = findViewById(R.id.activity_formulario_aluno_email);
+
+        Button botaoSalvar = findViewById(R.id.activity_formulario_aluno_botao_salvar);
+        botaoSalvar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String nome = campoNome.getText().toString();
+                String telefone = campoTelefone.getText().toString();
+                String email = campoEmail.getText().toString();
+
+                Aluno alunoCriado = new Aluno(nome, telefone, email);
+                dao.salva(alunoCriado);
+
+                startActivity(new Intent(FormularioAlunoActivity.this,
+                        ListaAlunosActivity.class));
+            }
+        });
     }
 }
--- app/src/main/java/br/gov/sp/fatec/agenda/ui/activity/ListaAlunosActivity.java
+++ app/src/main/java/br/gov/sp/fatec/agenda/ui/activity/ListaAlunosActivity.java
@@ -6,11 +6,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 import br.gov.sp.fatec.agenda.R;
+import br.gov.sp.fatec.agenda.dao.AlunoDAO;
 
 public class ListaAlunosActivity extends AppCompatActivity {
 
@@ -18,13 +15,14 @@ public class ListaAlunosActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_alunos);
+
+        AlunoDAO dao = new AlunoDAO();
         setTitle("Lista de Alunos");
-        List<String> alunos = new ArrayList<>(
-                Arrays.asList("Victor", "Serena", "Marilia", "Pablo", "Mariazinha", "Izaias", "Bibou"));
+
         ListView listaDeAlunos = findViewById(R.id.activity_lista_alunos_listview);
         listaDeAlunos.setAdapter(new ArrayAdapter<>(
                 this,
                 android.R.layout.simple_list_item_1,
-                alunos));
+                dao.todos()));
     }
 }
