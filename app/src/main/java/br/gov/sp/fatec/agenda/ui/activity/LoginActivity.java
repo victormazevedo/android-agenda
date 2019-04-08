6a60f3fb977d4b8001bb62611740d9749a6afda1
Victor Azevedo <victor_mouraazevedo@hotmail.com>
2019-04-08 11:51:10 -0300

    Criação de validação no onCLick do Login

--- app/src/main/java/br/gov/sp/fatec/agenda/ui/activity/LoginActivity.java
+++ app/src/main/java/br/gov/sp/fatec/agenda/ui/activity/LoginActivity.java
@@ -1,16 +1,80 @@
 package br.gov.sp.fatec.agenda.ui.activity;
 
 import android.app.Activity;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import br.gov.sp.fatec.agenda.R;
 
 public class LoginActivity extends Activity {
+    private static final String TAG = "LoginActivity";
+
+    private EditText campoEmail;
+    private EditText campoSenha;
+    private Button botaoLogar;
+    private TextView textoRegistrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        inicializacaoDosCampos();
+
+        botaoLogar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                login();
+            }
+        });
+    }
+
+    private void login() {
+        Log.d(TAG, "Login");
+
+        if (!validar()) {
+            loginFalhou();
+        }
+    }
+
+    private void loginFalhou() {
+        Toast.makeText(getBaseContext(), "Login falhou!", Toast.LENGTH_LONG).show();
+
+        botaoLogar.setEnabled(true);
+    }
+
+    private boolean validar() {
+        boolean valido = true;
+
+        String email = campoEmail.getText().toString();
+        String senha = campoSenha.getText().toString();
+
+        if (email.isEmpty() || !Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            campoEmail.setError("insira um e-mail válido!");
+            valido = false;
+        } else {
+            campoEmail.setError(null);
+        }
+
+        if (senha.isEmpty() || senha.length() < 4 || senha.length() > 15) {
+            campoSenha.setError("deve ser maior que 4 e menor que 15 caracteres");
+            valido = false;
+        } else {
+            campoSenha.setError(null);
+        }
+
+        return valido;
+    }
+
+    private void inicializacaoDosCampos() {
+        campoEmail = findViewById(R.id.activity_login_email);
+        campoSenha = findViewById(R.id.activity_login_senha);
+        botaoLogar = findViewById(R.id.activity_login_botao_logar);
+        textoRegistrar = findViewById(R.id.activity_login_registrar);
     }
 }
