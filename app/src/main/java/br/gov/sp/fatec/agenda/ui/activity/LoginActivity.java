b2892da75e728f187199718b6635e63549417f9c
Victor Azevedo <victor_mouraazevedo@hotmail.com>
2019-04-08 13:00:24 -0300

    Criação do ProgressDialog e refatoração

--- app/src/main/java/br/gov/sp/fatec/agenda/ui/activity/LoginActivity.java
+++ app/src/main/java/br/gov/sp/fatec/agenda/ui/activity/LoginActivity.java
@@ -1,19 +1,23 @@
 package br.gov.sp.fatec.agenda.ui.activity;
 
-import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import br.gov.sp.fatec.agenda.R;
 
-public class LoginActivity extends Activity {
+public class LoginActivity extends AppCompatActivity {
     private static final String TAG = "LoginActivity";
+    public static final String EMAIL = "victor.moura@fatec.com.br";
+    public static final String SENHA = "123456";
 
     private EditText campoEmail;
     private EditText campoSenha;
@@ -37,18 +41,57 @@ public class LoginActivity extends Activity {
     private void login() {
         Log.d(TAG, "Login");
 
-        if (!validar()) {
-            loginFalhou();
+        if (!validaCampos()) {
+            falhaLogin();
+            return;
         }
+
+        botaoLogar.setEnabled(false);
+
+        final ProgressDialog progressDialog = new ProgressDialog(LoginActivity.this,
+                R.style.AppTheme_Dark_Dialog);
+        progressDialog.setIndeterminate(true);
+        progressDialog.setMessage("Autenticando...");
+        progressDialog.show();
+
+        String email = campoEmail.getText().toString();
+        String senha = campoSenha.getText().toString();
+
+        if (email.equals(EMAIL) && senha.equals(SENHA)) {
+            sucessoLogin();
+        }
+
+        new Handler().postDelayed(
+                new Runnable() {
+                    @Override
+                    public void run() {
+                        falhaLogin();
+                        progressDialog.dismiss();
+                    }
+                }, 3000);
+
     }
 
-    private void loginFalhou() {
-        Toast.makeText(getBaseContext(), "Login falhou!", Toast.LENGTH_LONG).show();
+
+    private void sucessoLogin() {
+        botaoLogar.setEnabled(true);
+        startActivity(new Intent(this, ListaAlunosActivity.class));
+    }
+
+    private void falhaLogin() {
+        if (!campoSenha.getText().toString().equals(SENHA) && !campoEmail.getText().toString().equals(EMAIL)) {
+            campoSenha.setError("senha incorreta!");
+            campoEmail.setError("email não cadastrado!");
+        } else if (!campoEmail.getText().toString().equals(EMAIL)) {
+            campoEmail.setError("email não cadastrado!");
+        } else {
+            campoSenha.setError("senha incorreta!");
+        }
 
         botaoLogar.setEnabled(true);
     }
 
-    private boolean validar() {
+    private boolean validaCampos() {
         boolean valido = true;
 
         String email = campoEmail.getText().toString();
