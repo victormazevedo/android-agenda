ba8d8706799c9b656fe4294739a746c4715bdc19
Victor Azevedo <victor_mouraazevedo@hotmail.com>
2019-04-08 10:44:43 -0300

    Criação layout de login

---
+++ app/src/main/java/br/gov/sp/fatec/agenda/ui/activity/LoginActivity.java
@@ -0,0 +1,16 @@
+package br.gov.sp.fatec.agenda.ui.activity;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import br.gov.sp.fatec.agenda.R;
+
+public class LoginActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
