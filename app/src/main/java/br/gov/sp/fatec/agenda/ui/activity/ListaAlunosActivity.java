b3b87a8957385119a306f1f66d155d729c08a04d
Victor Azevedo <victor_mouraazevedo@hotmail.com>
2019-04-09 14:46:27 -0300

    Implementando o NavigationDrawer em ListaAlunos

--- app/src/main/java/br/gov/sp/fatec/agenda/ui/activity/ListaAlunosActivity.java
+++ app/src/main/java/br/gov/sp/fatec/agenda/ui/activity/ListaAlunosActivity.java
@@ -1,13 +1,22 @@
 package br.gov.sp.fatec.agenda.ui.activity;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import br.gov.sp.fatec.agenda.R;
 import br.gov.sp.fatec.agenda.dao.AlunoDAO;
@@ -16,15 +25,56 @@ public class ListaAlunosActivity extends AppCompatActivity {
 
     public static final String TITULO_APPBAR = "Lista de Alunos";
 
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle toggle;
+    private NavigationView navigationView;
+    private ActionBar actionBar;
+
     @Override
+
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_alunos);
-
         setTitle(TITULO_APPBAR);
+        actionBar = getSupportActionBar();
+        actionBar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#303F9F")));
 
+        drawerLayout = findViewById(R.id.activity_main);
+        toggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.Open, R.string.Close);
+
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        navigationView = findViewById(R.id.activity_lista_alunos_navigation_view);
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                int id = menuItem.getItemId();
+                switch (id) {
+                    case R.id.account:
+                        Toast.makeText(ListaAlunosActivity.this, "My Account", Toast.LENGTH_SHORT).show();
+                    case R.id.settings:
+                        Toast.makeText(ListaAlunosActivity.this, "Settings", Toast.LENGTH_SHORT).show();
+                    case R.id.info:
+                        Toast.makeText(ListaAlunosActivity.this, "My Cart", Toast.LENGTH_SHORT).show();
+                    default:
+                        return true;
+                }
+            }
+        });
         configuraFabNovoAluno();
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (toggle.onOptionsItemSelected(item))
+            return true;
+
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
